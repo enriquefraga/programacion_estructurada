22164eb7c23dee512f942dbf622164b1077db4b7
enriquefraga <enriquefraga15@gmail.com>
2020-01-27 21:37:59 -0600

    primera practica

---
+++ hola mundo.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main() {
+	printf ("hola mundo 1865815 \n");
+	return 0;
+}
